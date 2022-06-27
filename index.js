e97acf979aab85dab6baea61501d2af2ddc0a5ef
bill apiyo <billian177@gmail.com>
2022-06-27 09:40:10 +0300

    completed project

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let likeButton= document.getElementById("like");
     
     newCommentForm.addEventListener("submit", createNewComment);
-    likeButton.addEventListener("click", addLikeNum);
+    heart.addEventListener("click", likeCallback);
     
   });
   
@@ -91,12 +91,42 @@ document.addEventListener("DOMContentLoaded", () => {
   
   };
 
-  function addLikeNum(event){
-    let likeTotal=[0]
-    let numberNum= document.getElementById("likeNum")
-    event.forEach(()=> {
-        numberNum.innerText= likeTotal+1 
-    })
 
+ let EMPTY_HEART = '♡'
+ let FULL_HEART = '♥'
+
+ let heart = document.getElementById("like");
+
+  function likeCallback(e) {
+   let heart = e.target;
+    mimicServerCall("bogusUrl")
+      .then(function(){
+        if ( heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.className = "activated-heart";
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.className = "";
+        }
+      })
+      .catch(function(error) {
+       let modal = document.getElementById("modal");
+        modal.className = "";
+        modal.innerText = error;
+        setTimeout(() =>  modal.className = "hidden", 3000);
+      });
   }
   
+
+  function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 200);
+  });
+}
